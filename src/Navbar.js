b33e8e67134b6e081522662298117c88c94378d9
Emanuel Montenegro <montenegroemanuelsaxofonista@gmail.com>
2022-05-17 18:08:23 -0300

    nav

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
 
 <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">ART & DECO</a>
+    <a className="navbar-brand" href="#">ARTDECO</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
